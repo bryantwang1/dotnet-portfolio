c4f119f4febd5f978e814eb8ed2faf8e13dfd307
Bryant <bryant.m.wang@gmail.com>
2017-03-03 13:59:11 -0800

    Add basic github api call.

--- src/dotnetPortfolioSite/Controllers/HomeController.cs
+++ src/dotnetPortfolioSite/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using dotnetPortfolioSite.Models;
 
 namespace dotnetPortfolioSite.Controllers
 {
@@ -20,16 +24,34 @@ namespace dotnetPortfolioSite.Controllers
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Projects()
         {
-            ViewData["Message"] = "Your contact page.";
-
+            var client = new RestClient("https://api.github.com/");
+            var request = new RestRequest("zen");
+            client.AddDefaultHeader("Authorization", "token " + EnvironmentVariables.AuthToken);
+            request.AddHeader("User-Agent", "Epicoduc Portfolio Project");
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            //JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            ViewBag.myString = response.Content.ToString();
             return View();
         }
 
         public IActionResult Error()
         {
             return View();
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
         }
     }
 }
---
+++ src/dotnetPortfolioSite/Models/EnvironmentVariables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetPortfolioSite.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AuthToken = "1508b56e809dcafae9f331490f4392190e11e5e3";
+    }
+}
