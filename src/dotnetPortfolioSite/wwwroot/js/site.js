f04e996d603a811f82ccb78d7d988d099fa396e8
Bryant <bryant.m.wang@gmail.com>
2017-03-03 15:49:07 -0800

    Add some styling and animation to projects page, add more animation to index page, add content to about page.

--- src/dotnetPortfolioSite/wwwroot/js/site.js
+++ src/dotnetPortfolioSite/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿function HorizontalAnimateString(str, target) {
+﻿function VerticalFadeIn(str, target) {
     var chars = str.split("");
 
     $(target).append("<div class='row-flexer'></div>");
@@ -31,6 +31,57 @@
     removeClasses();
 }
 
+function HorizontalSlideFadeIn(str, target) {
+    var chars = str.split("");
+
+    $(target).append("<div class='row-flexer'></div>");
+
+    chars.forEach(function (char, idx) {
+        if (char === " ") {
+            $(target).children(".row-flexer").append("<h2>&nbsp</h2>");
+        } else {
+            $(target).children(".row-flexer").append("<h2 class='bw-fade-in-char bw-anim-x-positive-offset bw-opacity-0' id='animation2-id-" + idx + "'>" + char + "<h2>");
+        }
+    });
+
+    var counter = 0;
+    function removeClasses() {
+        setTimeout(function () {
+            if (counter < chars.length) {
+                $("#animation2-id-" + counter).removeClass("bw-opacity-0");
+                $("#animation2-id-" + counter).removeClass("bw-anim-x-positive-offset");
+                removeClasses();
+            }
+            counter++;
+        }, 220);
+    }
+
+    removeClasses();
+}
+
 $(function () {
-    HorizontalAnimateString("Bryant Wang's Portfolio Site", ".animation-area");
+    HorizontalSlideFadeIn("Welcome To", ".animation-area");
+    
+    $(window).bind("load", function () {
+        setTimeout(function () {
+            VerticalFadeIn("Bryant Wang's Portfolio Site", ".animation-area-2");
+        }, 2500);
+    });
+
+    $(window).bind("load", function () {
+        setTimeout(function () {
+            var counter = 1;
+            function removeClasses() {
+                setTimeout(function () {
+                    if (counter <= 3) {
+                        $("#panel-" + counter).removeClass("bw-opacity-0");
+                        removeClasses();
+                    }
+                    counter++;
+                }, 300);
+            }
+
+            removeClasses();
+        }, 500);
+    });
 });
