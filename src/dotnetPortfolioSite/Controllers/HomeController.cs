8d2fab92f60d669d24b638604ea91c1a6257168b
Bryant <bryant.m.wang@gmail.com>
2017-03-03 15:08:46 -0800

    Create model GitProject, move api call to GitProject, modify call to do a proper search and return a list of GitProjects, change display of results on Projects view.

--- src/dotnetPortfolioSite/Controllers/HomeController.cs
+++ src/dotnetPortfolioSite/Controllers/HomeController.cs
@@ -19,39 +19,18 @@ namespace dotnetPortfolioSite.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Projects()
         {
-            var client = new RestClient("https://api.github.com/");
-            var request = new RestRequest("zen");
-            client.AddDefaultHeader("Authorization", "token " + EnvironmentVariables.AuthToken);
-            request.AddHeader("User-Agent", "Epicoduc Portfolio Project");
-            var response = new RestResponse();
-            Task.Run(async () =>
-            {
-                response = await GetResponseContentAsync(client, request) as RestResponse;
-            }).Wait();
-            //JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
-            ViewBag.myString = response.Content.ToString();
-            return View();
+            List<GitProject> projectList = GitProject.GetProjects();
+            return View(projectList);
         }
 
         public IActionResult Error()
         {
             return View();
-        }
-
-        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
-        {
-            var tcs = new TaskCompletionSource<IRestResponse>();
-            theClient.ExecuteAsync(theRequest, response => {
-                tcs.SetResult(response);
-            });
-            return tcs.Task;
         }
     }
 }
---
+++ src/dotnetPortfolioSite/Models/GitProject.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetPortfolioSite.Models
+{
+    public class GitProject
+    {
+        public string HtmlUrl { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        public static List<GitProject> GetProjects()
+        {
+            var client = new RestClient("https://api.github.com/");
+            var request = new RestRequest("search/repositories");
+            request.AddParameter("q", "@bryantwang1");
+            request.AddParameter("sort", "stars");
+            request.AddParameter("order", "desc");
+            request.AddParameter("per_page", "3");
+            client.AddDefaultHeader("Authorization", "token " + EnvironmentVariables.AuthToken);
+            request.AddHeader("User-Agent", "Epicodus Portfolio Project");
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var items = jsonResponse["items"];
+            var projectList = new List<GitProject> { };
+            
+            for(int i = 0;i < items.Count();i++)
+            {
+                GitProject newProject = new GitProject();
+                newProject.Name = items[i]["name"].ToString();
+                newProject.HtmlUrl = items[i]["html_url"].ToString();
+                newProject.Description = items[i]["description"].ToString();
+                projectList.Add(newProject);
+            }
+            //ViewBag.myString = response.Content.ToString();
+            return projectList;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
