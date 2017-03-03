a8b77b235b7378375305bf0e8eb0da1bf75ac1e3
Bryant <bryant.m.wang@gmail.com>
2017-03-03 13:22:59 -0800

    Add custom-css classes and js function to allow easy animation of strings.

--- src/dotnetPortfolioSite/wwwroot/js/site.js
+++ src/dotnetPortfolioSite/wwwroot/js/site.js
@@ -1,1 +1,36 @@
-﻿// Write your Javascript code.
+﻿function HorizontalAnimateString(str, target) {
+    var chars = str.split("");
+
+    $(target).append("<div class='row-flexer'></div>");
+
+    chars.forEach(function (char, idx) {
+        if (char === " ") {
+            $(target).children(".row-flexer").append("<h2>&nbsp</h2>");
+        } else {
+            if (idx % 2 == 0) {
+                $(target).children(".row-flexer").append("<h2 class='bw-fade-in-char bw-anim-y-positive-offset bw-opacity-0' id='animation-id-" + idx + "'>" + char + "<h2>");
+            } else {
+                $(target).children(".row-flexer").append("<h2 class='bw-fade-in-char bw-anim-y-negative-offset bw-opacity-0' id='animation-id-" + idx + "'>" + char + "<h2>");
+            }
+        }
+    });
+
+    var counter = 0;
+    function removeClasses() {
+        setTimeout(function () {
+            if (counter < chars.length) {
+                $("#animation-id-" + counter).removeClass("bw-opacity-0");
+                $("#animation-id-" + counter).removeClass("bw-anim-y-positive-offset");
+                $("#animation-id-" + counter).removeClass("bw-anim-y-negative-offset");
+                removeClasses();
+            }
+            counter++;
+        }, 220);
+    }
+
+    removeClasses();
+}
+
+$(function () {
+    HorizontalAnimateString("Bryant Wang's Portfolio Site", ".animation-area");
+});
