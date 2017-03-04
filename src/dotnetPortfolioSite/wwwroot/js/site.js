1f70a501c0e59e69136ffa107aa166cf89862232
Bryant <bryant.m.wang@gmail.com>
2017-03-03 16:50:42 -0800

    Add experimental square animation to index.

--- src/dotnetPortfolioSite/wwwroot/js/site.js
+++ src/dotnetPortfolioSite/wwwroot/js/site.js
@@ -57,10 +57,61 @@ function HorizontalSlideFadeIn(str, target) {
     }
 
     removeClasses();
+
+}
+
+function AnimateSquares(target) {
+    $(target).append("<div class='row-flexer'></div>");
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+    var lastColorIndex = -1;
+
+    console.log($(target).width());
+    var squareCount = Math.floor($(target).innerWidth() / 100);
+    for (var i = 0; i <= squareCount; i++) {
+        var colorIndex = -1;       
+        do {
+            colorIndex = Math.floor(Math.random() * colors.length);
+        }
+        while (colorIndex == lastColorIndex);
+        lastColorIndex = colorIndex;
+        $(target).children(".row-flexer").append("<div class='bw-anim-square bw-anim-x-complete-pos-offset " + colors[colorIndex] + "-bg' id='square-" + i + "'></div>");
+    }
+
+    var counter = 0;
+    function removeClasses() {
+        setTimeout(function () {
+            if (counter <= squareCount) {
+                $("#square-" + counter).removeClass("bw-anim-x-complete-pos-offset");
+                removeClasses();
+            }
+            counter++;
+        }, 220);
+    }
+
+    removeClasses();
+
+    setTimeout(function () {
+        var counter2 = 0;
+        function addClasses() {
+            setTimeout(function () {
+                if (counter2 <= squareCount) {
+                    $("#square-" + counter2).addClass("bw-anim-x-complete-neg-offset");
+                    addClasses();
+                } else {
+                    $(target).empty();
+                }
+                counter2++;
+            }, 220);
+        }
+        
+        addClasses();
+    }, squareCount * 221);
+
 }
 
 $(function () {
     HorizontalSlideFadeIn("Welcome To", ".animation-area");
+    AnimateSquares(".animation-area-3");
     
     $(window).bind("load", function () {
         setTimeout(function () {
@@ -82,6 +133,6 @@ $(function () {
             }
 
             removeClasses();
-        }, 500);
+        }, 100);
     });
 });
